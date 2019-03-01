0d2ecd8c52345154e428835765cf057c8884e42c
Tariq <bilal.tariq@accenture.com>
2019-03-01 00:07:11 +0000

    Searching functionality (State Management)

--- src/CardList.js
+++ src/CardList.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Card from './Card'
 
 const CardList = ({robots}) => {
-    const cardsArray = robots.map( (user, i) => {
-        return (
-            <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email} />
-        )
-    });
+    
     return (
         <div>
-            {cardsArray}
+            {
+            robots.map((robot, i) => {
+                return (
+                    <Card 
+                        key={robot.id} 
+                        id={robot.id} 
+                        name={robot.name} 
+                        email={robot.email} 
+                    />
+                )
+            })
+            }
         </div>
     );
 }
